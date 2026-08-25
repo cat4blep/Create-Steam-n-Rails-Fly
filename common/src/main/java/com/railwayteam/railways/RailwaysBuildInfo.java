9c6784d39a8a419a0de3dd359e9a4821e0056f34
cat4blep <kotik@devyan.ru>
2026-08-25 21:02:27 +0300

    Release SNR.FLY-STABLE-1.0.1
    Publish the startup-crash hotfix under a new patch version while preserving the existing SNR.FLY-STABLE-1.0 tag and artifact.

--- common/src/main/java/com/railwayteam/railways/RailwaysBuildInfo.java
+++ common/src/main/java/com/railwayteam/railways/RailwaysBuildInfo.java
@@ -1,7 +1,7 @@
 package com.railwayteam.railways;
 
 public final class RailwaysBuildInfo {
-    public static final String VERSION = "SNR.FLY-STABLE-1.0";
+    public static final String VERSION = "SNR.FLY-STABLE-1.0.1";
     public static final String GIT_COMMIT = "local-port";
     public static final boolean INCLUDE_DEV_COMMANDS = false;
 
